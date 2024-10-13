31dbafc496aa3a9786944970baba0b8e1cc3f959
kaliganoff <kaliganoff@yahoo.com>
2024-10-13 17:06:49 +0300

    feat: Add cd

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import os from "os";
 import path from "path";
 import fs from 'fs';
+import { spawn } from "child_process";
 
 const { stdin, stdout } = process;
 let dir = os.homedir();
+process.chdir(dir);
 
 
 const username = process.env.npm_config_username;
 if (username) {
     console.log(`Welcome to the File Manager, ${username}!`);
-    console.log('You are currently in ' + dir);
+    console.log('You are currently in ' + process.cwd());
     console.log('Please enter commands: ');
 } else {
     console.log('Please specify your username as an argument like this: ')
@@ -20,15 +22,26 @@ if (username) {
 stdin.on('data', (data) => {
     if (data.toString() === 'up' + os.EOL) {
         dir = path.join(dir, '..');
+        process.chdir(dir);
         console.log('You are currently in ' + dir);
     }
     if (data.toString() === 'ls' + os.EOL) {
         fs.readdir(dir, { withFileTypes: true }, (err, dirents) => {
-            if (err) throw new Error('FS operation failed');
+            if (err) throw new Error('Operation failed');
             let directories = dirents.filter(item => item.isDirectory()).map(item => [item.name, 'directory']);
             let files = dirents.filter(item => item.isFile()).map(item => [item.name, 'file']);
             console.table([...directories, ...files])
           });
+    }
+    if (data.toString().includes('cd')) {
+        const newDir = data.toString().split(' ')[1];
+        try {
+        process.chdir(newDir.split(os.EOL)[0]);
+        dir = process.cwd();
+        console.log(process.cwd())
+        } catch(error) {
+            console.log('Invalid input')
+        }
     }
 });
 
