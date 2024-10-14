cf7b4eb2bdc0e97d72fe6d868b2035ce6d49cd66
kaliganoff <kaliganoff@yahoo.com>
2024-10-14 14:30:07 +0300

    feat: Add fs operations

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import os from "os";
 import path from "path";
 import fs from 'fs';
-import { spawn } from "child_process";
 
 const { stdin, stdout } = process;
 let dir = os.homedir();
@@ -27,22 +26,98 @@ stdin.on('data', (data) => {
     }
     if (data.toString() === 'ls' + os.EOL) {
         fs.readdir(dir, { withFileTypes: true }, (err, dirents) => {
-            if (err) throw new Error('Operation failed');
+            if (err) console.log('Operation failed');
             let directories = dirents.filter(item => item.isDirectory()).map(item => [item.name, 'directory']);
             let files = dirents.filter(item => item.isFile()).map(item => [item.name, 'file']);
             console.table([...directories, ...files])
           });
     }
-    if (data.toString().includes('cd')) {
+    if (data.toString().startsWith('cd')) {
         const newDir = data.toString().split(' ')[1];
         try {
         process.chdir(newDir.split(os.EOL)[0]);
         dir = process.cwd();
-        console.log(process.cwd())
+        console.log('You are currently in ' + dir)
         } catch(error) {
             console.log('Invalid input')
         }
     }
+    if (data.toString().startsWith('cat')) {
+        const filePath = data.toString().split(' ')[1].split(os.EOL)[0];
+        try {
+        const readStream = fs.createReadStream(filePath, 'utf-8');
+        readStream.on('data', (chunk) => stdout.write(chunk));
+        readStream.on('end', () => stdout.write(os.EOL));
+        } catch(error) {
+            console.log('Invalid input')
+        }
+    }
+    if (data.toString().startsWith('add')) {
+        const fileName = data.toString().split(' ')[1].split(os.EOL)[0];
+        fs.writeFile(fileName, '', { flag: 'wx' }, err => {
+            if (err) {
+              console.log('Operation failed');
+            } else {
+                console.log('File created')
+            }
+          });
+    }
+    if (data.toString().startsWith('rn')) {
+        const filePath = data.toString().split(' ')[1];
+        const newName = data.toString().split(' ')[2].split(os.EOL)[0];
+        const newPath = path.join(path.dirname(filePath), newName);
+        const exists = fs.existsSync(newPath);
+        if (!exists) {
+        fs.promises.rename(filePath, newPath).then(() => console.log('File renamed successfully')).catch(() => {
+            console.error('FS operation failed')
+        })
+    } else {
+        console.error('FS operation failed')
+    }
+    }
+    if (data.toString().startsWith('cp')) {
+        try {
+        const filePath = data.toString().split(' ')[1];
+        const fileName = path.basename(filePath);
+        const newDir = data.toString().split(' ')[2].split(os.EOL)[0];
+            const readStream = fs.createReadStream(filePath, 'utf-8');
+            const writeStream = fs.createWriteStream(path.join(newDir + fileName), 'utf-8');
+            readStream.pipe(writeStream);
+            readStream.on('error', () => {
+                console.log('1')
+            });
+    } catch(error) {
+        console.log(error);
+    }
+}
+if (data.toString().startsWith('mv')) {
+    try {
+        const filePath = data.toString().split(' ')[1];
+        const fileName = path.basename(filePath);
+        const newDir = data.toString().split(' ')[2].split(os.EOL)[0];
+            const readStream = fs.createReadStream(filePath, 'utf-8');
+            const writeStream = fs.createWriteStream(path.join(newDir + fileName), 'utf-8');
+            readStream.pipe(writeStream);
+            readStream.on('error', () => {
+                console.log(1);
+            });
+            readStream.on('close', () => {
+                fs.unlink(filePath, (err) => {
+                    if (err) console.error('Operation failed');
+                  }); 
+            })
+    } catch(error) {
+        console.log('Invalid input');
+    }
+}
+if (data.toString().startsWith('rm')) {
+    try {
+        const filePath = data.toString().split(' ')[1].split(os.EOL)[0];
+        fs.unlink(filePath, (err) => {
+          if (err) console.error('Operation failed');
+        }); 
+    } catch (error) {
+        console.error(error);
+    }
+}
 });
-
-
